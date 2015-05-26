cb02430af58fae1bab52df8a80f59d12cbc839c1
Bjorn Orri <bjornorri@gmail.com>
2015-05-26 11:54:00 +0200

    Create UI for vote code activity

--- app/src/main/java/eu/eurescom/evote/VoteCodeActivity.java
+++ app/src/main/java/eu/eurescom/evote/VoteCodeActivity.java
@@ -2,37 +2,33 @@ package eu.eurescom.evote;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class VoteCodeActivity extends Activity {
+
+    private EditText mCodeField;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote_code);
+
+        mCodeField = (EditText) findViewById(R.id.codeField);
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                didPressSubmitButton();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vote_code, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void didPressSubmitButton() {
     }
 }
