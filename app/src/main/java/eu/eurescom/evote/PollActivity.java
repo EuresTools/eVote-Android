c808294a44c3070df9aa5cee3cb7c6c78c0e6b34
Bjorn Orri <bjornorri@gmail.com>
2015-05-28 11:58:36 +0200

    Selection working fine

--- app/src/main/java/eu/eurescom/evote/PollActivity.java
+++ app/src/main/java/eu/eurescom/evote/PollActivity.java
@@ -1,13 +1,22 @@
 package eu.eurescom.evote;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import eu.eurescom.evote.Model.Poll;
 
@@ -16,22 +25,82 @@ public class PollActivity extends Activity {
 
     private ListView mListView;
     private Poll mPoll;
-    private ArrayAdapter mAdapter;
+    private PollListAdapter mAdapter;
+
+    private HashSet<Integer> votes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll);
 
+        votes = new HashSet<Integer>();
         mListView = (ListView) findViewById(R.id.optionsList);
         mPoll = (Poll) getIntent().getExtras().getParcelable("poll");
-        mAdapter = new ArrayAdapter(this, R.layout.cell_option, R.id.optionLabel, mPoll.getOptions());
+        mAdapter = new PollListAdapter(this, R.layout.cell_option, R.id.optionLabel, mPoll.getOptions());
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(PollActivity.this, "Clicked item at position " + position, Toast.LENGTH_SHORT).show();
+                Log.d("", "Clicked item at position " + position);
+                CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+
+                int select = mPoll.getSelect();
+                // If this is a single choice poll, move the selection.
+                if(select == 1 ) {
+                    if(votes.contains(position)) {
+                        votes.remove(position);
+                    }
+                    else {
+                        votes.clear();
+                        votes.add(position);
+                    }
+                }
+                else {
+                    // Prevent the user from selecting more options than allowed.
+                    int count = votes.size();
+                    if(count < select || votes.contains(position)) {
+                        if(votes.contains(position)) {
+                            votes.remove(position);
+                        }
+                        else {
+                            votes.add(position);
+                        }
+                    }
+                    else {
+                        // User tried to select more options than allowed.
+                        new AlertDialog.Builder(PollActivity.this)
+                                .setTitle("Not allowed")
+                                .setMessage("You cannot select more than " + select + " options.")
+                                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+
+                                    }
+                                })
+                                .show();
+                    }
+                }
+                updateUI();
             }
         });
+    }
+
+    private void updateUI() {
+        mAdapter.notifyDataSetChanged();
+    }
+
+    private class PollListAdapter extends ArrayAdapter<String> {
+        public PollListAdapter(Context context, int resource, int textViewResourceId, List<String> objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+            checkBox.setChecked(votes.contains(position));
+            return view;
+        }
     }
 }
