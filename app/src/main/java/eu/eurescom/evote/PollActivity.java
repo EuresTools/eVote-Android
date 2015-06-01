3209e7c6c073590166ea2b3b59a3fe7108400422
Bjorn Orri <bjornorri@gmail.com>
2015-06-01 15:30:39 +0200

    Unused imports

--- app/src/main/java/eu/eurescom/evote/PollActivity.java
+++ app/src/main/java/eu/eurescom/evote/PollActivity.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
